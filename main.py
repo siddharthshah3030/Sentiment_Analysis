2e75ba8e9f933bf6c98a40a1f74495cf9339eff2
siddharth shah <siddharthshah3030@gmail.com>
2019-02-02 14:53:46 +0530

    removing expressions and getting pkain words

--- main.py
+++ main.py
@@ -3,3 +3,22 @@
 import pandas as pd       
 train = pd.read_csv("labeledTrainData.tsv", header=0, \
                     delimiter="\t", quoting=3)
+
+# beautifulSoup import and removal of HTML tags
+from bs4 import BeautifulSoup    
+
+example1 = BeautifulSoup(train["review"][0])  
+print(train["review"][0])
+print(example1.get_text())
+
+#import re to remove numbers and other expressions except a-z
+import re
+letters_only = re.sub("[^a-zA-Z]",           # The pattern to search for
+                      " ",                   # The pattern to replace it with
+                      example1.get_text() )  # The text to search
+
+print (letters_only)
+
+
+lower_case = letters_only.lower()        # Convert to lower case
+words = lower_case.split()               # Split into words
